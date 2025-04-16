37a681698bb9f629af94995da19ad706299a5118
siyamak chehreh <siyamak.chehreh73@gmail.com>
2025-04-16 10:01:08 -0700

    responsive design for AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,48 +2,48 @@ import mountain from "../assets/mountain.jpg";
 
 export default function AboutPage() {
   return (
-    <section id="about">
-      <div>
-        <div className="flex flex-col gap-20 overflow-hidden overflow-y-auto md:flex md:flex-row mx-auto my-auto justify-around items-center h-screen bg-gray-200 perspective-normal">
-          <div className="h-130 w-110 border-7 border-stone-400 rounded-xl bg-amber-50 flex flex-col place-items-center opacity-45 hover:opacity-100 transform-3d hover:rotate-y-3 transition-all duration-700 hover:scale-[1.04] shadow-2xl">
+    <section id="about" className="bg-gray-200">
+      <div className="py-4">
+        <div className="flex flex-col gap-20 basis-auto overflow-hidden overflow-y-auto md:flex md:flex-row mx-auto my-auto justify-around items-center h-screen bg-gray-200 perspective-normal">
+          <div className="h-90 w-70 md:h-130 md:w-110 border-7 border-stone-400 rounded-xl bg-amber-50 flex flex-col place-items-center opacity-45 hover:opacity-100 transform-3d hover:rotate-y-3 transition-all duration-700 hover:scale-[1.04]  shadow-2xl">
             <img
-              className="w-80 mx-15 -mt-15 border-7 rounded-lg border-double border-stone-500 shadow-2xl"
+              className="w-40 md:w-80 mx-15 -mt-15 border-7 rounded-lg border-double border-stone-500 shadow-2xl"
               src={mountain}
             />
-            <h1 className="text-3xl font-boldonse font-bold my-12">
+            <h1 className="text-lg md:text-3xl font-boldonse font-bold my-4 md:my-12">
               Interior Design
             </h1>
-            <p className="text-xl/8 font-underdog text-justify mx-4">
+            <p className="text-md mt-2 md:text-xl/8 font-underdog text-justify mx-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Praesentium, laudantium earum. Magnam laboriosam totam, cumque,
               quis laborum non nulla, unde maxime pariatur quas officiis eaque
               aut fuga qui aliquid ratione.
             </p>
           </div>
-          <div className="h-130 w-110 border-7 border-stone-400 rounded-xl bg-amber-50 flex flex-col place-items-center opacity-45 hover:opacity-100 transform-3d transition duration-700 hover:scale-[1.04] shadow-2xl">
+          <div className="h-90 w-70 md:h-130 md:w-110 border-7 border-stone-400 rounded-xl bg-amber-50 flex flex-col place-items-center opacity-45 hover:opacity-100 transform-3d transition duration-700 hover:scale-[1.04]  shadow-2xl">
             <img
-              className="w-80 mx-15 -mt-15 border-7 border-double rounded-lg border-stone-500 shadow-2xl"
+              className="w-40 md:w-80 mx-15 -mt-15 border-7 border-double rounded-lg border-stone-500 shadow-2xl"
               src={mountain}
             />
-            <h1 className="text-3xl font-boldonse font-bold my-12">
+            <h1 className="text-lg md:text-3xl font-boldonse font-bold my-4 md:my-12">
               Facade Design
             </h1>
-            <p className="text-xl/8 font-underdog text-justify mx-4">
+            <p className="text-md mt-2 md:text-xl/8 font-underdog text-justify mx-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Praesentium, laudantium earum. Magnam laboriosam totam, cumque,
               quis laborum non nulla, unde maxime pariatur quas officiis eaque
               aut fuga qui aliquid ratione.
             </p>
           </div>
-          <div className="h-130 w-110 border-7 border-stone-400 rounded-xl bg-amber-50 flex flex-col place-items-center opacity-45 hover:opacity-100 transform-3d hover:-rotate-y-3 transition duration-700 hover:scale-[1.04] shadow-2xl">
+          <div className="h-90 w-70 md:h-130 md:w-110 border-7 border-stone-400 rounded-xl bg-amber-50 flex flex-col place-items-center opacity-45 hover:opacity-100 transform-3d hover:-rotate-y-3 transition duration-700 hover:scale-[1.04]  shadow-2xl">
             <img
-              className="w-80 mx-15 -mt-15 border-7 border-double rounded-lg border-stone-500 shadow-2xl"
+              className="w-40 md:w-80 mx-15 -mt-15 border-7 border-double rounded-lg border-stone-500 shadow-2xl"
               src={mountain}
             />
-            <h1 className="text-3xl font-boldonse font-bold my-12">
+            <h1 className="text-lg md:text-3xl font-boldonse font-bold my-4 md:my-12">
               Consultant
             </h1>
-            <p className="text-xl/8 font-underdog text-justify mx-4">
+            <p className="text-md mt-2 md:text-xl/8 font-underdog text-justify mx-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Praesentium, laudantium earum. Magnam laboriosam totam, cumque,
               quis laborum non nulla, unde maxime pariatur quas officiis eaque
--- src/pages/ArchitectsPage.tsx
+++ src/pages/ArchitectsPage.tsx
@@ -3,105 +3,102 @@ import mountain from "../assets/mountain.jpg";
 export default function ArchitectsPage() {
   return (
     <section id="architects">
-      <div>
-        <div className="flex flex-col h-screen w-screen">
-          <div className="h-75 w-screen bg-gray-200 flex flex-row gap-15 justify-center items-center perspective-normal">
-            <div className="h-60 w-75 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300 rounded-xl relative hover:rotate-y-360 transform-3d transition-all duration-1000 shadow-2xl">
-              <img
-                className="h-58 w-73 top-1 left-1 rounded-xl absolute"
-                src={mountain}
-              />
+      <div className="flex flex-col h-screen w-screen">
+        <div className="h-45 md:h-75 gap-5 md:gap-15 w-screen bg-gray-200 flex flex-row  justify-center items-center perspective-normal">
+          <div className="h-25 w-35 md:h-60 md:w-75 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300 rounded-xl relative hover:rotate-y-360 transform-3d transition-all duration-1000 shadow-2xl">
+            <img
+              className="h-23 w-33 md:h-58 md:w-73 top-1 left-1 rounded-xl absolute"
+              src={mountain}
+            />
+          </div>
+          <div className="h-25 w-75 md:h-60 md:w-170 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300 rounded-xl relative shadow-2xl">
+            <div className="flex flex-col h-23 w-73 md:h-58 md:w-168 bg-amber-50 rounded-lg md:gap-5 px-2 py-1 absolute top-1 left-1 scale-[0.97] hover:scale-[1.00] transition-all duration-700 shadow-2xl">
+              <h1 className="font-lalezar font-bold text-center text-sm md:text-2xl pt-0 md:pt-2">
+                روح الله احمدی
+              </h1>
+              <h1 className="font-lalezar text-center font-bold text-sm md:text-lg">
+                مدیر عامل شرکت آرکا دیزاین
+              </h1>
+              <p className="font-lalezar text-center text-xs md:text-lg">
+                مهندس روح الله احمدی فارغ التحصیل مقطع کارشناسی رشته ی معماری از
+                دانشگاه تهران و مقطع کارشناسی ارشد از دانشگاه هنر لصفهان در سال
+                1385 می باشد.
+              </p>
             </div>
-            <div className="h-60 w-170 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300 rounded-xl relative shadow-2xl">
-              <div className="flex flex-col h-58 w-168 bg-amber-50 rounded-lg gap-5 px-2 py-1 absolute top-1 left-1 scale-[0.97] hover:scale-[1.00] transition-all duration-700 shadow-2xl">
-                <h1 className="font-boldonse font-bold text-2xl pt-2">
-                  Siyamak Chehreh
+          </div>
+        </div>
+        <div className="h-104 w-screen bg-gray-200">
+          <div className="h-104 w-screen flex flex-row items-end justify-center gap-28">
+            <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
+              <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
+                <img
+                  className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
+                  src={mountain}
+                />
+              </div>
+              <div className="flex flex-col mt-18 mr-2">
+                <h1 className="text-right font-boldonse">Thomas Party</h1>
+                <h1 className="text-right font-boldonse text-sm my-2 mr-4">
+                  3D Designer
                 </h1>
-                <h1 className="font-boldonse font-bold text-lg">
-                  CEO of Architecture Company
-                </h1>
-                <p className="font-underdog text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-                  tempore, tempora asperiores iste ipsum adipisci dignissimos
-                  excepturi commodi eaque, distinctio, dolorem maiores velit
-                  provident fugiat aliquam dolor nostrum animi sit!
+                <p className="text-center font-underdog">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur veritatis laboriosam illum at perspiciatis.
                 </p>
               </div>
             </div>
-          </div>
-          <div className="h-104 w-screen bg-gray-200">
-            <div className="h-104 w-screen flex flex-row items-end justify-center gap-28">
-              <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
-                <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
-                  <img
-                    className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
-                    src={mountain}
-                  />
-                </div>
-                <div className="flex flex-col mt-18 mr-2">
-                  <h1 className="text-right font-boldonse">Thomas Party</h1>
-                  <h1 className="text-right font-boldonse text-sm my-2 mr-4">
-                    3D Designer
-                  </h1>
-                  <p className="text-center font-underdog">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consequuntur veritatis laboriosam illum at perspiciatis.
-                  </p>
-                </div>
+            <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
+              <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
+                <img
+                  className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
+                  src={mountain}
+                />
               </div>
-              <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
-                <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
-                  <img
-                    className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
-                    src={mountain}
-                  />
-                </div>
-                <div className="flex flex-col mt-18 mr-2">
-                  <h1 className="text-right font-boldonse">Thomas Party</h1>
-                  <h1 className="text-right font-boldonse text-sm my-2 mr-4">
-                    3D Designer
-                  </h1>
-                  <p className="text-center font-underdog">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consequuntur veritatis laboriosam illum at perspiciatis.
-                  </p>
-                </div>
+              <div className="flex flex-col mt-18 mr-2">
+                <h1 className="text-right font-boldonse">Thomas Party</h1>
+                <h1 className="text-right font-boldonse text-sm my-2 mr-4">
+                  3D Designer
+                </h1>
+                <p className="text-center font-underdog">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur veritatis laboriosam illum at perspiciatis.
+                </p>
               </div>
-              <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
-                <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
-                  <img
-                    className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
-                    src={mountain}
-                  />
-                </div>
-                <div className="flex flex-col mt-18 mr-2">
-                  <h1 className="text-right font-boldonse">Thomas Party</h1>
-                  <h1 className="text-right font-boldonse text-sm my-2 mr-4">
-                    3D Designer
-                  </h1>
-                  <p className="text-center font-underdog">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consequuntur veritatis laboriosam illum at perspiciatis.
-                  </p>
-                </div>
+            </div>
+            <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
+              <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
+                <img
+                  className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
+                  src={mountain}
+                />
               </div>
-              <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
-                <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
-                  <img
-                    className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
-                    src={mountain}
-                  />
-                </div>
-                <div className="flex flex-col mt-18 mr-2">
-                  <h1 className="text-right font-boldonse">Thomas Party</h1>
-                  <h1 className="text-right font-boldonse text-sm my-2 mr-4">
-                    3D Designer
-                  </h1>
-                  <p className="text-center font-underdog">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consequuntur veritatis laboriosam illum at perspiciatis.
-                  </p>
-                </div>
+              <div className="flex flex-col mt-18 mr-2">
+                <h1 className="text-right font-boldonse">Thomas Party</h1>
+                <h1 className="text-right font-boldonse text-sm my-2 mr-4">
+                  3D Designer
+                </h1>
+                <p className="text-center font-underdog">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur veritatis laboriosam illum at perspiciatis.
+                </p>
+              </div>
+            </div>
+            <div className="border-10 border-fuchsia-100 rounded-lg bg-stone-300 h-70 w-60 mb-10 relative perspective-normal opacity-60 hover:opacity-100 hover:scale-[1.1] transition-all duration-800 drop-shadow-2xl">
+              <div className="absolute bg-amber-600 h-45 w-45 rounded-full -top-18 -left-20 transform-3d hover:rotate-x-360 transition-all duration-800 animate-rotate-border bg-conic/[from_var(--border-angle)] from-stone-300 via-amber-400 to-stone-300">
+                <img
+                  className="h-43 w-43 left-1 right-1 top-1 rounded-full object-cover absolute "
+                  src={mountain}
+                />
+              </div>
+              <div className="flex flex-col mt-18 mr-2">
+                <h1 className="text-right font-boldonse">Thomas Party</h1>
+                <h1 className="text-right font-boldonse text-sm my-2 mr-4">
+                  3D Designer
+                </h1>
+                <p className="text-center font-underdog">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur veritatis laboriosam illum at perspiciatis.
+                </p>
               </div>
             </div>
           </div>
